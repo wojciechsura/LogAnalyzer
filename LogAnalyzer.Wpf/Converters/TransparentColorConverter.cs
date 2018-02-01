8c275de9f5a68b17fbb5ed0ff302e6669364b242
Wojciech "Spook" Sura <wojciechsura@gmail.com>
2018-02-01 21:23:48 +0100

    Cosmetic changes + icon

---
+++ LogAnalyzer.Wpf/Converters/TransparentColorConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace LogAnalyzer.Wpf.Converters
+{
+    public class TransparentColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is Color color)
+            {
+                return new SolidColorBrush(Color.FromArgb(128, color.R, color.G, color.B));
+            }
+            else
+                return Binding.DoNothing;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
